20e9c09e6734b865edde76addaa4c1f72197335f
sujin-park <psujin831@gmail.com>
2021-05-03 22:02:34 +0900

    Add: 5장 - 01. 프로젝트 준비 및 기본적인 사용법

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Users from './Users';
+import { Route, Link } from 'react-router-dom';
 import { UsersProvider } from './UsersContext';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <UsersProvider>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/about">소개</Link>
+          </li>
+        </ul>
+        <hr />
+      </div>
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
       <Users />
     </UsersProvider>
   );
