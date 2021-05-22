897748a9282add19b886ed6b0113d13745c7cdbe
sujin-park <psujin831@gmail.com>
2021-05-22 10:50:31 +0900

    Add: 7장-22. redux-saga select 로 현재 상태 조회하기

--- redux-middleware/src/containers/PostContainer.js
+++ redux-middleware/src/containers/PostContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getPost, goToHome } from '../modules/posts';
+import { getPost, goToHome, printState } from '../modules/posts';
 import Post from '../components/Post';
 import { reducerUtils } from '../lib/asyncUtils';
 
@@ -20,6 +20,7 @@ function PostContainer({ postId }) {
   return (
     <>
       <button onClick={() => dispatch(goToHome())}>홈으로 이동</button>
+      <button onClick={() => dispatch(printState())}>상태 출력</button>
       <Post post={data} />
     </>
   );
--- redux-middleware/src/modules/posts.js
+++ redux-middleware/src/modules/posts.js
@@ -1,6 +1,6 @@
 import * as postsAPI from '../api/posts';
 import { reducerUtils, createPromiseThunk, handleAsyncActions, handleAsyncActionsById, createPromiseSaga, createPromiseSagaById } from '../lib/asyncUtils';
-import { takeEvery, getContext } from 'redux-saga/effects';
+import { takeEvery, getContext, select } from 'redux-saga/effects';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -12,10 +12,11 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 
 const CLEAR_POST = 'CLEAR_POST';
 const GO_TO_HOME = 'GO_TO_HOME';
+const PRINT_STATE = 'PRINT_STATE';
 
 export const getPosts = () => ({ type: GET_POSTS });
 export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id });
-
+export const printState = () => ({ type: PRINT_STATE });
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
 
@@ -25,10 +26,18 @@ function* goToHomeSaga() {
   history.push('/');
 }
 
+function* printStateSaga() {
+  // saga 에서 현재 상태에 따라 조건부 로직을 작성할 때 select 를 이용하면 현재 상태를 확인할 수 있다.
+  const state = yield select(state => state.posts);
+
+  console.log(state);
+}
+
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
   yield takeEvery(GO_TO_HOME, goToHomeSaga);
+  yield takeEvery(PRINT_STATE, printStateSaga);
 }
 
 export const goToHome = () => ({ type: GO_TO_HOME });
